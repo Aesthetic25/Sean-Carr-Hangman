2ee262add02314063b6ce6d417ca4a29b004a972
Sean Tenedero <seancarr2005@gmail.com>
2021-09-09 09:19:34 +0800

    refined some code

--- script/script.js
+++ script/script.js
@@ -15,10 +15,10 @@ function importJSON(file, arrayToAppendTo){
     $.getJSON(file, (jsonData)=>{
         for(let i = 0; i<jsonData.length; i++){
             arrayToAppendTo.push(jsonData[i]);
-        }
+        };
         return arrayToAppendTo;
-    })
-}
+    });
+};
 //calls the function to push the json data to an array declared earlier.
 importJSON("/script/countries.json", countriesList);
 importJSON("/script/animals.json", animalsList);
@@ -37,32 +37,32 @@ class Hangman{
         this.animalsList = animals;
         this.namesList = names;
         this.clear();
-    }
+    };
     clear(){
         this.mysteryWordText.innerHTML = "_ _ _ _";
         this.lives = 10;
         this.livesText.innerHTML = `You have ${this.lives} lives left.`;
         this.resetButtonColor();
         this.status = "disabled";
-    }
+    };
     restart(){
         this.mysteryWordText.innerHTML = "_ _ _ _"
         this.lives = 10;
         this.livesText.innerHTML = `You have ${this.lives} lives left.`;
         this.resetButtonColor();
         this.status = "disabled";
-    }
+    };
     resetButtonColor(){
         this.alphabetButtons.forEach((button)=>{
             button.style.backgroundColor = "#187bcd";
             button.style.color = "#fff";
         })
-    }
+    };
     changeButtonColor(button){
         if(button.style.backgroundColor == "rgb(255, 255, 255)") return true;
         button.style.backgroundColor = "#fff";
         button.style.color = "#187bcd";
-    }
+    };
     play(){
         this.restart();
         this.status = "enabled"
@@ -70,13 +70,13 @@ class Hangman{
         let randomArrayElement = (array)=>{
             let randomPickedNumber = Math.floor(Math.random() * array.length);
             return array[randomPickedNumber];
-        }
+        };
         let chosenCategory = randomArrayElement(categories);
         let underscorePrinter = (array)=>{
             let finalUnderscoreString = [];
             array.forEach((arrayVal)=> (arrayVal === "-") ? (finalUnderscoreString.push("- ")) : (finalUnderscoreString.push("_ ")));
             return finalUnderscoreString;
-        }
+        };
         switch(chosenCategory){
             case "countries":
                 this.chosenWord = randomArrayElement(this.countriesList).replace(/\s/g, "-").split("");
@@ -98,13 +98,13 @@ class Hangman{
                 break;
             default:
                 return;
-        }
-    }
+        };
+    };
     alphabetButtonActivated(buttonLetter, buttonElement){
         if(this.status == "disabled"){
             this.clear();
             return true;
-        }
+        };
         if(buttonElement.style.backgroundColor == "#fff") return;
         let temporaryTextPlaceholder = this.mysteryWordText.innerHTML.replace(/\s/g, "").split("");
         if(this.chosenWord.includes(buttonLetter) || this.chosenWord.includes(buttonLetter.toLowerCase())){
@@ -118,24 +118,21 @@ class Hangman{
             console.log("wrong letter")//
             this.lives--;
             (this.lives == 1) ? (this.livesText.innerHTML = `You have ${this.lives} life left.`) : (this.livesText.innerHTML = `You have ${this.lives} lives left.`);
-        }
+        };
         if(this.lives == 0){
             setTimeout(() => {alert(`Game over, the word was "${this.chosenWord.join("")}"`)}, 100);
             this.status = "disabled"
             this.clear();
             return;
-        }
-        if(this.mysteryWordText.innerHTML.includes("_" || " _" || "_ ")){
-            return;
-        }else{
-            console.log(this.chosenWord.join(""))//
-            setTimeout(() => alert(`You win, the word was "${this.chosenWord.join("")}"`), 100);
-            this.status = "disabled";
-            this.clear();
-            return;
-        }
-    }
-}
+        };
+        if(this.mysteryWordText.innerHTML.includes("_" || " _" || "_ ")) return;
+        console.log(this.chosenWord.join(""))//
+        setTimeout(() => alert(`You win, the word was "${this.chosenWord.join("")}"`), 100);
+        this.status = "disabled";
+        this.clear();
+        return;
+    };
+};
 //creates new instance of the class "Hangman"
 const hangman = new Hangman(categoryText, mysteryWordText, livesText, playButton, restartButton, alphabetButtons, countriesList, animalsList, namesList);
 //adding event listeners to the buttons.
@@ -146,6 +143,6 @@ alphabetButtons.forEach(button => {
             if(hangman.changeButtonColor(button)==true) return;
             hangman.changeButtonColor(button);
             hangman.alphabetButtonActivated(button.innerHTML, button);
-        })
+        });
     }
-)
+);
